5995c41244b12c8d924e63d1d9232014cd531ec9
luulet <sampowes@metropolia.fi>
2025-01-16 10:24:04 +0200

    lisäsin moduuli2 tehtävä 4

---
+++ moduuli2/tehtava4.py
@@ -0,0 +1,12 @@
+luku1_str = input("Anna ensimmäinen kokonaisluku: ")
+luku2_str = input("Anna toinen kokonaisluku: ")
+luku3_str = input("Anna kolmas kokonaisluku: ")
+luku1 = int(luku1_str)
+luku2 = int(luku2_str)
+luku3 = int(luku3_str)
+summa = luku1 + luku2 + luku3
+tulo = luku1 * luku2 * luku3
+keski = (luku1 + luku2 + luku3) / 3
+print("Lukujen summa on " + str(summa) + ".")
+print("Lukujen tulo on " + str(tulo) + ".")
+print("Lukujen keskiarvo on " + str(keski) + ".")
