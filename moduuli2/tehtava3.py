018e2aa2ba77d82e5dabd606c37b326ae2bd2757
luulet <sampowes@metropolia.fi>
2025-01-16 10:07:42 +0200

    päivitin moduuli2 tehtävä 3

--- moduuli2/tehtava3.py
+++ moduuli2/tehtava3.py
@@ -1,3 +1,8 @@
 kanta_str = input("Anna suorakulman kanta: ")
 korkeus_str = input("Anna suorakulman korkeus: ")
-piiri
+kanta = int(kanta_str)
+korkeus = int(korkeus_str)
+piiri = (kanta * 2) + (korkeus * 2)
+pinta = kanta * korkeus
+print("Suorakulmion piiri on " + str(piiri) + ".")
+print("Suorakulmion pinta-ala on " + str(pinta) + ".")
