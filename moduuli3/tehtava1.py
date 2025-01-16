99ff9172445a8dc72e4cad019ab4a937535729e7
luulet <sampowes@metropolia.fi>
2025-01-16 13:32:39 +0200

    tein moduuli3 tehtävä 1

---
+++ moduuli3/tehtava1.py
@@ -0,0 +1,9 @@
+kala = float(input("Anna kuhan pituus senttimetreinä: "))
+if kala>=37:
+    print("Kuha on hyvä!")
+else:
+    puuttuu = 37 - kala
+    print("Kuha on " + str(puuttuu) + " senttiä alamittainen, laske kuha takaisin järveen." )
+
+
+
