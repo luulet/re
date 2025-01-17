a7915af7ac61cdd318ca0216a3dc0abdc262c739
luulet <sampowes@metropolia.fi>
2025-01-17 20:56:01 +0200

    tein moduuli3 tehtävä 3

--- moduuli3/tehtava3.py
+++ moduuli3/tehtava3.py
@@ -1,3 +1,16 @@
 suku = input("Anna sukupuolesi (mies/nainen): ")
-hemo = input("Anna hemoglobiiniarvo (g/l): ")
+hemo = float(input("Anna hemoglobiiniarvo (g/l): "))
 if suku=="mies":
+    if 134>hemo:
+        print("Hemoglobiiniarvo on alhainen.")
+    elif 134<=hemo<=195:
+        print("Hemoglobiiniarvo on normaali.")
+    elif hemo>195:
+        print("Hemoglobiiniarvo on korkea.")
+if suku=="nainen":
+    if 117>hemo:
+        print("Hemoglobiiniarvo on alhainen.")
+    elif 117<=hemo<=175:
+        print("Hemoglobiiniarvo on normaali.")
+    elif hemo>175:
+        print("Hemoglobiiniarvo on korkea.")
