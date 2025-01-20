f795652bcae4f679b99e2bb0e3464d59810c8543
luulet <sampowes@metropolia.fi>
2025-01-20 11:01:53 +0200

    tein moduuli3 tehtävä 4

--- moduuli3/tehtava4.py
+++ moduuli3/tehtava4.py
@@ -1,8 +1,13 @@
 print("Selvitetään onko annettu vuosiluku karkausvuosi vai ei.")
 vuosi = float(input("Anna vuosiluku: "))
-jako = float(vuosi%4 == 0)
-if (vuosi==jako):
-    print("joo")
+if vuosi % 4 == 0:
+    if vuosi % 100 == 0:
+        if vuosi % 400 == 0:
+            print("Vuosi on karkausvuosi.")
+        else:
+            print("Vuosi ei ole karkausvuosi.")
+    else:
+        print("Vuosi on karkausvuosi.")
 else:
-    print("häh")
+    print("Vuosi ei ole karkausvuosi")
 
