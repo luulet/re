a7763318f48acd18d0067dd02f5a43002fa0ba63
luulet <sampowes@metropolia.fi>
2025-01-30 14:46:50 +0200

    tehty 6 moduuliin tehtäviä

--- moduuli6/tehtava6.py
+++ moduuli6/tehtava6.py
@@ -4,6 +4,15 @@ def main():
     dia1 = float(input(" > "))
     print("Anna ensimmäisen pizzan hinta euroina: ")
     cost1 = float(input(" > "))
+    print("Anna toisen pizzan halkaisija senttimetreinä: ")
+    dia2 = float(input(" > "))
+    print("Anna toisen pizzan hinta euroina: ")
+    cost2 = float(input(" > "))
+    print(pizza(value1))
+    return dia1
 
-
+def pizza(pizza):
+    import math
+    value1 = (math.pi/4)*pow(dia1,2)
+    return value1
 main()
