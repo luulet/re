f9c3faf15db903b59d4ad9424b0758a8dcf7a574
luulet <sampowes@metropolia.fi>
2025-02-04 15:31:21 +0200

    päivitys

--- moduuli7/tehtava3.py
+++ moduuli7/tehtava3.py
@@ -1,12 +1,15 @@
 def main():
 
-
-    while True:
+    hit = True
+    airports = {}
+    airports = set(#nimet)
+    while hit:
         print("Haluatko syöttää uuden lentoaseman (1), hakea lentoaseman tiedot (2) tai lopettaa (Enter)")
         option_input = input(" > ")
         if option_input == "1":
 
-
+        if option_input == (""):
+            hit = False
         print("Icao: ")
         icao_input = input(" > ")
 
