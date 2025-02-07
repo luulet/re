f0c2968d8d3a1795bd68c14136d1f768259c679d
luulet <sampowes@metropolia.fi>
2025-02-07 13:56:55 +0200

    moduuli 8 tehtävä 2 päivitys

--- moduuli8/tehtava2.py
+++ moduuli8/tehtava2.py
@@ -13,6 +13,8 @@ yhteys = mysql.connector.connect(
 
 
 def main(country):
+
+
     sql = f"SELECT type FROM airport where iso_country='{country}'"
     kursori = yhteys.cursor()
     kursori.execute(sql)
@@ -21,22 +23,37 @@ def main(country):
     pienkentta = 0
     kentta = 0
     isokentta = 0
+    heli = 0
+    sea = 0
+
 
     for airport in tulos:
         if airport[0] == "small_airport":
             pienkentta += 1
     for airport in tulos:
-        if airport[0] == "airport":
+        if airport[0] == "medium_airport":
             kentta += 1
     for airport in tulos:
         if airport[0] == "large_airport":
             isokentta += 1
-
+    for airport in tulos:
+        if airport[0] == "heliport":
+            heli += 1
+    for airport in tulos:
+        if airport[0] == "seaplane_base":
+            sea += 1
     if kursori.rowcount > 0:
-        print(pienkentta, kentta, isokentta)
-        return pienkentta, kentta, isokentta
+        if isokentta > 0:
+            print(f'Isoja lentokenttiä on {isokentta}.')
+        if kentta > 0:
+            print(f'Keskikokoisia lentokenttiä on {kentta}.')
+        if pienkentta > 0:
+            print(f"Pienlentokenttiä on {pienkentta}.")
+        if heli > 0:
+            print(f'Helikopterikenttiä on {heli}.')
+        if sea > 0:
+            print(f'Vesilentokoneen tukikohtia on {sea}.')
 
-def type():
 
 
 
@@ -44,3 +61,4 @@ def type():
 print("Anna maakoodi, ja ohjelma tulostaa lentokenttien määrät tyypeittäin.")
 country = input(" > ")
 main(country)
+
